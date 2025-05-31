4058fb136de69235220efea16734f0e1c18849f0
thoriqdharmawan <thoriqd21@gmail.com>
2025-06-01 01:41:04 +0700

    feat: enhance Footer component with undo/redo buttons and update styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,5 @@
+import Button from '@/components/Button';
+import Icon from '@/components/icon';
 import { FC, ReactNode } from 'react';
 import { View } from 'react-native';
 import { styles } from './style';
@@ -7,7 +9,25 @@ interface FooterProps {
 }
 
 const Footer: FC<FooterProps> = ({ children }) => {
-  return <View style={styles.container}>{children}</View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.historyWrapper}>
+        <Button
+          icon={<Icon library='MaterialCommunityIcons' name='undo' />}
+          style={styles.button}
+          size='small'
+          variant='ghost'
+        />
+        <Button
+          icon={<Icon library='MaterialCommunityIcons' name='redo' />}
+          style={styles.button}
+          size='small'
+          variant='ghost'
+        />
+      </View>
+      {children}
+    </View>
+  );
 };
 
 export default Footer;
--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,11 +1,23 @@
-import { Colors, Layout } from '@/constants';
+import { Colors, Layout, Spacing } from '@/constants';
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  button: {
+    margin: Spacing.margin.none,
+    padding: Spacing.padding.none,
+  },
   container: {
     alignItems: 'center',
     backgroundColor: Colors.white,
     height: Layout.footerHeight,
     justifyContent: 'center',
+    position: 'relative',
+  },
+  historyWrapper: {
+    flexDirection: 'row',
+    gap: Spacing.sm,
+    left: Spacing.md,
+    position: 'absolute',
+    top: -Spacing.xxl,
   },
 });
