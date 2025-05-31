d7c081b13efe40c70d5b537e3fb20cd4df30dd6a
thoriqdharmawan <thoriqd21@gmail.com>
2025-05-31 13:27:35 +0700

    feat: add text elements to MEME_TEMPLATES for enhanced meme customization

--- src/constants/templates.ts
+++ src/constants/templates.ts
@@ -36,10 +36,46 @@ const ELEMENTS_DEMO_MEME1: CanvasTextElement[] = [
   },
 ];
 
+const ELEMENTS_DEMO_MEME3: CanvasTextElement[] = [
+  {
+    id: 'el-demo-meme-1',
+    text: 'Thoriq Dharmawan',
+    x: 28.260417938232422,
+    y: 145.96503067016602,
+    width: 132.63653155735562,
+    color: '#000000',
+    fontWeight: '800',
+    textAlign: 'center',
+    fontSize: 22,
+  },
+  {
+    id: 'el-demo-meme-2',
+    text: 'Kandidat Lain',
+    x: 245.66703605651855,
+    y: 138.6294641494751,
+    width: 85.56807967594693,
+    color: '#000000',
+    fontWeight: '800',
+    textAlign: 'center',
+    fontSize: 17,
+  },
+  {
+    id: 'el-demo-meme-3',
+    text: 'Lahelu',
+    x: 166.32106149196625,
+    y: 101,
+    width: 95.77046176365445,
+    color: '#000000',
+    fontWeight: '800',
+    textAlign: 'center',
+    fontSize: 22,
+  },
+];
+
 export const MEME_TEMPLATES: MemeTemplate[] = [
   { id: '1', source: meme1, elemets: ELEMENTS_DEMO_MEME1 },
   { id: '2', source: meme2 },
-  { id: '3', source: meme3 },
+  { id: '3', source: meme3, elemets: ELEMENTS_DEMO_MEME3 },
   { id: '12', source: meme1 },
   { id: '22', source: meme2 },
   { id: '32', source: meme3 },
